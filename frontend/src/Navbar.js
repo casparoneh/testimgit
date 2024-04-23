a268760d12012fa3badd354d61fbcd8547ff8234
Lorik Vitia <yamada_lorri@hotmail.com>
2024-04-23 20:55:05 +0200

    adding navbar

---
+++ frontend/src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <div>
+        <ul>
+            <li>asd</li>
+            <li>asda</li>
+            <li>123</li>
+            <li>12</li>
+            <li>123</li>
+        </ul>
+    </div>
+  )
+}
+export default Navbar
